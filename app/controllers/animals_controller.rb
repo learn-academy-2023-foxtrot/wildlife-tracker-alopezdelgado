2efc8418c1ee6e3428b01b05358066b96176db3d
alopezdelgado <andrealdelgado_99@outlook.com>
2023-10-06 09:48:42 -0700

    added a method to delete an animal entry

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -20,6 +20,14 @@ class AnimalsController < ApplicationController
           render json: animals.errors
         end
       end
+      def destroy
+        animal = Animal.find(params[:id])
+        if animal.destroy
+          render json: animal
+        else
+          render json: animal.errors
+        end
+      end
      private 
      def animals_params 
         params.require(:animal).permit(:name, :scientific_binomial)
